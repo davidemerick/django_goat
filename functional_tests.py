bc2b4351ad82b6ec6a2581e779e1e9ffc5adea9b
DavidEmerick <d.emerick@outlook.com>
2018-09-03 12:53:32 -0700

    Added helper function to repeat tests without multiple same code blocks

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app
         # She goes to check out the home page
@@ -38,9 +43,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         # "1: Brainstorm backlog" as an item in a to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Brainstorm/Setup initial backlog creation session', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Brainstorm/Setup initial backlog creation session')
 
         # There is still a text box where she is able to add another item
         # She enters "Schedule backlog creation session"
@@ -50,13 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Brainstorm/Setup initial backlog creation session',
-        [row.text for row in rows])
-        self.assertIn(
-            '2: Schedule backlog creation session', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Brainstorm/Setup initial backlog creation session')
+        self.check_for_row_in_list_table('2: Schedule backlog creation session')
 
         # Edith wonders whether the site will remember her list
         # She sees that the site has generated a unique URL for her
