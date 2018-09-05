60c20543f70d5876a357414769c2dc31fbd122fd
DavidEmerick <d.emerick@outlook.com>
2018-09-05 10:09:39 -0700

    remove time.sleep, change check_for_row to wait_for_row

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,9 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 import time
 
@@ -12,10 +15,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    # Helper method to wait for row
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app
@@ -43,7 +55,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         # "1: Brainstorm backlog" as an item in a to-do list
-        self.check_for_row_in_list_table('1: Brainstorm/Setup initial backlog creation session')
+        self.wait_for_row_in_list_table('1: Brainstorm/Setup initial backlog creation session')
 
         # There is still a text box where she is able to add another item
         # She enters "Schedule backlog creation session"
@@ -53,8 +65,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # The page updates and now shows both items on her list
-        self.check_for_row_in_list_table('1: Brainstorm/Setup initial backlog creation session')
-        self.check_for_row_in_list_table('2: Schedule backlog creation session')
+        self.wait_for_row_in_list_table('1: Brainstorm/Setup initial backlog creation session')
+        self.wait_for_row_in_list_table('2: Schedule backlog creation session')
 
         # Edith wonders whether the site will remember her list
         # She sees that the site has generated a unique URL for her
